c402ab680abe9c572b15ebc830f73e8d1629634c
Keith Staines <keith.staines@workfinder.com>
2021-12-07 16:38:11 +0000

    Initial Commit

---
+++ SwiftUICapsuleCollectiin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICapsuleCollectiin
+//
+//  Created by Keith Staines on 07/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICapsuleCollectiin/SwiftUICapsuleCollectiinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICapsuleCollectiinApp.swift
+//  SwiftUICapsuleCollectiin
+//
+//  Created by Keith Staines on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICapsuleCollectiinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
